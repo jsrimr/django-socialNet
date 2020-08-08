932633c6be527157a2260774d5eb320d40204535
jsrimr <jsrimr@naver.com>
2020-08-08 18:30:57 +0900

    form submit하면 POST 되도록 어떻게 따로 조치를 해야하나? 버튼 눌러도 반응이 없네

--- profiles/views.py
+++ profiles/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
-from .models import Profile
+
 # Create your views here.
+from .forms import ProfileModelForm
+from .models import Profile
+
 
 def my_profile_view(request):
-    obj = Profile.objects.get(user=request.user)
-
+    profile = Profile.objects.get(user=request.user)
+    form = ProfileModelForm(request.POST or None, request.FILES or None, instance=profile)
     # obj = Profile.objects.first()
+    confirm = False
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+            confirm = True
+
     context = {
-        'obj' : obj
+        'profile': profile,
+        'form': form,
+        'confirm': confirm,
     }
 
     return render(request, 'profiles/myprofile.html', context)
